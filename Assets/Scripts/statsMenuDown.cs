c06bd3f471b3bcb422135a3cfe60e4b36fcf0025
Bryan Ramirez <brami007@fiu.edu>
2015-07-01 13:52:57 -0400

    Stats menu functional

---
+++ Assets/Scripts/statsMenuDown.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class statsMenuDown : MonoBehaviour {
+	Animator anime;
+	public bool menuDown;
+	//bool checkMenuDown;
+
+	// Use this for initialization
+	void Start () {
+		anime = GameObject.Find("statsWindow").GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	
+	}
+
+	public void onMenuDown()
+	{
+			anime.SetTrigger ("menuDown");
+			menuDown = true;
+		
+	}
+
+}
---
+++ Assets/Scripts/statsMenuUp.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class statsMenuUp : MonoBehaviour {
+	Animator anime;
+
+	
+	// Use this for initialization
+	void Start () {
+		anime = GameObject.Find("statsWindow").GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	public void OnMouseDown() {
+
+
+		anime.SetTrigger("menuUp");
+		Debug.Log ("test");
+
+	}
+
+
+	// Use this for initialization
+
+}
